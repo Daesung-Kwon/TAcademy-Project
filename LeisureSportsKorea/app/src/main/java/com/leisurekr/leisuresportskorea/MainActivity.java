f693d5d4286eda18343df3250f398f0f860368a0
dlddldwkak <dlddldwkak@gmail.com>
2017-05-12 13:49:15 +0900

    test용으로 커밋

--- LeisureSportsKorea/app/src/main/java/com/leisurekr/leisuresportskorea/MainActivity.java
+++ LeisureSportsKorea/app/src/main/java/com/leisurekr/leisuresportskorea/MainActivity.java
@@ -11,9 +11,7 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        for(int i=0;i<2;i++){
-
-        }
+        android.widget.Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
 
 
     }
