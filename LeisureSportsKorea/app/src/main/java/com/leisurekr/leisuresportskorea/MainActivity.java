f6ab878ca581ad81de1933fae02b722469d38fa7
Daesung-Kwon <simadeit@naver.com>
2017-05-25 10:33:58 +0900

    Second Tab Layout(RecyclerView) 수정

--- LeisureSportsKorea/app/src/main/java/com/leisurekr/leisuresportskorea/MainActivity.java
+++ LeisureSportsKorea/app/src/main/java/com/leisurekr/leisuresportskorea/MainActivity.java
@@ -1,13 +1,21 @@
 package com.leisurekr.leisuresportskorea;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Explode;
+import android.transition.Transition;
+import android.view.Window;
 import android.widget.Toast;
 
 
@@ -15,53 +23,78 @@ public class MainActivity extends AppCompatActivity{
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private SwipeRefreshLayout refreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 커스텀 툴바(액션바) 추가 in Main Activity
+        // Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // 탭레이아웃 초기화 및 추가
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        tabLayout.addTab(tabLayout.newTab().setText("Tab One"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab Two"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab Three"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab Four"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab FIve"));
+        // Tab Layout
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tap_homeimage_view));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tap_shopimage_view));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tap_mypageimage_view));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        // 뷰페이저 초기화
-        viewPager = (ViewPager) findViewById(R.id.pager);
+        // View Pager
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        // 뷰페이저 어답터 생성
+        // Adapter For View Pager
         TabPagerAdapter pagerAdapter = new TabPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(pagerAdapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
-        // Set TabSelectedListener
+        // Set Tab Selected Listener
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
 
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
             }
-
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
 
             }
-
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-
+                Toast.makeText(MainActivity.this, "re-tapped", Toast.LENGTH_SHORT).show();
             }
         });
 
-        android.widget.Toast.makeText(this, "123", Toast.LENGTH_SHORT).show();
+        refreshLayout = (SwipeRefreshLayout) findViewById(R.id.refreshlayout);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // TO-DO : Refreshing List
 
+                        refreshLayout.setRefreshing(false);
+                    }
+                }, 2500);
+            }
+        });
+        refreshLayout.setColorSchemeColors(Color.BLUE, Color.RED, Color.GREEN);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            Transition exitTrans = new Explode(); // Fade(), Slide()
+
+            Transition reenterTrans = new Explode(); // Fade(), Slide()
+
+            window.setExitTransition(exitTrans);
+            window.setReenterTransition(reenterTrans);
+            // window.setTransitionBackgroundFadeDuration(2000);
+            window.setAllowEnterTransitionOverlap(true);
+            window.setAllowReturnTransitionOverlap(true);
+        }
     }
+
+    Handler mHandler = new Handler(Looper.getMainLooper());
 }
--- LeisureSportsKorea/app/src/main/java/com/leisurekr/leisuresportskorea/TabPagerAdapter.java
+++ LeisureSportsKorea/app/src/main/java/com/leisurekr/leisuresportskorea/TabPagerAdapter.java
@@ -2,25 +2,22 @@ package com.leisurekr.leisuresportskorea;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by mobile on 2017. 5. 11..
  */
 
-public class TabPagerAdapter extends FragmentStatePagerAdapter {
-
-    // 탭카운트 변수
-    private int tabCount;
+public class TabPagerAdapter extends FragmentPagerAdapter {
 
     public TabPagerAdapter(FragmentManager fm, int tabCount) {
         super(fm);
-        this.tabCount = tabCount;
     }
 
     @Override
     public Fragment getItem(int position) {
-
         // 현재 탭 위치의 Tab Fragment 리턴
         switch (position) {
             case 0:
@@ -32,19 +29,12 @@ public class TabPagerAdapter extends FragmentStatePagerAdapter {
             case 2:
                 TabFragment3 tabFragment3 = new TabFragment3();
                 return tabFragment3;
-            case 3:
-                TabFragment4 tabFragment4 = new TabFragment4();
-                return tabFragment4;
-            case 4:
-                TabFragment5 tabFragment5 = new TabFragment5();
-                return tabFragment5;
             default:
                 return null;
         }
     }
-
     @Override
     public int getCount() {
-        return tabCount;
+        return 3;
     }
 }
